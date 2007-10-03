abb441fb8affc40700fe7d2c9264c7516dea5f1b
Quinton Reeves <qreeves@gmail.com>
2007-10-03 14:59:55 +0000

    Former-commit-id: 4d6ee900ccf8f1e5710434efad49c269edfcfbe8

--- bloodfrontier.bat
+++ bloodfrontier.bat
@@ -5,12 +5,12 @@ set BF_DIR=.
 set BF_HOME=home
 set BF_OPTIONS=-q%BF_HOME% -r -k%BF_DIR%
 
-IF EXIST engine\bloodfrontier_client.exe (
-	engine\bloodfrontier_client.exe %BF_OPTIONS% %*
+IF EXIST bin\bloodfrontier_client.exe (
+	bin\bloodfrontier_client.exe %BF_OPTIONS% %*
 ) ELSE (
-	IF EXIST %BF_DIR%\engine\bloodfrontier_client.exe (
+	IF EXIST %BF_DIR%\bin\bloodfrontier_client.exe (
 		pushd %BF_DIR%
-		engine\bloodfrontier_client.exe %BF_OPTIONS% %*
+		bin\bloodfrontier_client.exe %BF_OPTIONS% %*
 		popd
 	) ELSE (
 		echo Unable to find the Blood Frontier client
