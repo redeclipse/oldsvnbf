75df58101308792c0abd0426a1856ebd8a0b8172
Quinton Reeves <qreeves@gmail.com>
2007-10-07 02:12:53 +0000

    pragma warning disable 4800 on vcpp
    Former-commit-id: 3375335f2da18bf8099ee52c5d9bb31425fd310d

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -64,6 +64,9 @@ typedef unsigned int uint;
 #pragma warning (disable: 4267) // conversion from 'size_t' to 'int', possible loss of data
 #pragma warning (disable: 4355) // 'this' : used in base member initializer list
 #pragma warning (disable: 4996) // 'strncpy' was declared deprecated
+#ifdef BFRONTIER
+#pragma warning (disable: 4800) // forcing value to bool 'true' or 'false' (performance warning)
+#endif
 #endif
 
 #define strcasecmp _stricmp
