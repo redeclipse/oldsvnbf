b552afc7521112f3f7487ff6ed6523b0c7fb6c28
acord <acord@0d5a7262-033b-0410-87d9-8ba89202c25b>
2007-10-27 04:49:54 +0000

    Former-commit-id: e8e731c46b15a37cea7e02cddd32b56703d6a8d9

--- bloodfrontier.bat
+++ bloodfrontier.bat
@@ -3,7 +3,7 @@
 set SDL_VIDEO_WINDOW_POS=0,0
 set BF_DIR=.
 set BF_HOME=home
-set BF_OPTIONS=-q%BF_HOME% -r -k%BF_DIR% -P -t
+set BF_OPTIONS=-q%BF_HOME% -r -k%BF_DIR% -P 
 
 IF EXIST bin\bloodfrontier_client.exe (
 	bin\bloodfrontier_client.exe %BF_OPTIONS% %* 
