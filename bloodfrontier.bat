ed3c23062584d6118b56c6b6a981eebf9c41fcc2
Quinton Reeves <qreeves@gmail.com>
2007-12-05 06:57:16 +0000

    Former-commit-id: ee06af71bfb5b8402aa2e86005807e90591947bb

--- bloodfrontier.bat
+++ bloodfrontier.bat
@@ -6,7 +6,7 @@ set BF_HOME=home
 set BF_OPTIONS=-q%BF_HOME% -r
 
 IF EXIST bin\bloodfrontier_client.exe (
-	bin\bloodfrontier_client.exe -t %BF_OPTIONS% %* 
+	bin\bloodfrontier_client.exe %BF_OPTIONS% %* 
 ) ELSE (
 	IF EXIST %BF_DIR%\bin\bloodfrontier_client.exe (
 		pushd %BF_DIR%
