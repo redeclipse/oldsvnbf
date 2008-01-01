5fd62662d1f0493e6b7973a76f63e2a15f0eb83c
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-01-01 21:43:08 +0000

    Former-commit-id: de1aea7087394d0f04c81230ae2cab584a36542a

--- src/engine/shadowmap.cpp
+++ src/engine/shadowmap.cpp
@@ -571,10 +571,10 @@ void rendershadowmap()
             }
             else
             {
-            glTexCoord2f(0, 0); glVertex2f(-1, -1);
-            glTexCoord2f(1, 0); glVertex2f(1, -1);
-            glTexCoord2f(1, 1); glVertex2f(1, 1);
-            glTexCoord2f(0, 1); glVertex2f(-1, 1);
+				glTexCoord2f(0, 0); glVertex2f(-1, -1);
+				glTexCoord2f(1, 0); glVertex2f(1, -1);
+				glTexCoord2f(1, 1); glVertex2f(1, 1);
+				glTexCoord2f(0, 1); glVertex2f(-1, 1);
             }
             glEnd();
 
