4191212e8555c00f5ec90bbb4ca55d9666e70ccb
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-01-15 09:24:08 +0000

    single way links made red teleport ranges made 0..360 and -89..89 temporary init.cfg hack
    Former-commit-id: 9b22dbbd0a1d3b62b8d18d69f091bdcae4fd2c18

--- bloodfrontier.bat
+++ bloodfrontier.bat
@@ -3,7 +3,7 @@
 set SDL_VIDEO_WINDOW_POS=0,0
 set BF_DIR=.
 set BF_HOME=home
-set BF_OPTIONS=-q%BF_HOME% -r
+set BF_OPTIONS=-q%BF_HOME% -rbfa\init.cfg
 
 IF EXIST bin\bloodfrontier_client.exe (
 	bin\bloodfrontier_client.exe %BF_OPTIONS% %* 
