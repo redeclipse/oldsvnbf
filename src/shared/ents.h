5579e471c47ace3e49e34d0d73607bb909b3df53
Lee Salzman <lsalzman@gmail.com>
2008-02-10 09:52:30 +0000

    make sure floor value is sane
    Former-commit-id: 7ba78e3499fc6e630a538ec2c39cbc4f1b938b3c

--- src/shared/ents.h
+++ src/shared/ents.h
@@ -79,6 +79,7 @@ struct physent                                  // base entity type, can be affe
         timeinair = strafe = move = 0;
         physstate = PHYS_FALL;
 		vel = gvel = vec(0, 0, 0);
+        floor = vec(0, 0, 1);
     }
 };
 
