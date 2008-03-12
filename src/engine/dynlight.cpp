d586df227bdf8873b377694b535d9a12c7aadae7
Lee Salzman <lsalzman@gmail.com>
2008-03-12 19:03:33 +0000

    dynlight collision optimization
    Former-commit-id: f3464fdf25ab6e54d55cf75355943d6b5db3cede

--- src/engine/dynlight.cpp
+++ src/engine/dynlight.cpp
@@ -93,13 +93,20 @@ void updatedynlights()
 int finddynlights()
 {
     closedynlights.setsizenodelete(0);
-    if(maxdynlights) loopvj(dynlights)
+    if(!maxdynlights) return 0;
+    physent e;
+    e.type = ENT_CAMERA;
+    loopvj(dynlights)
     {
         dynlight &d = dynlights[j];
         if(d.curradius <= 0) continue;
         d.dist = camera1->o.dist(d.o) - d.curradius;
         if(d.dist > dynlightdist || isvisiblesphere(d.curradius, d.o) >= VFC_FOGGED || pvsoccluded(d.o, 2*int(d.curradius+1))) 
             continue;
+
+        e.o = d.o;
+        e.radius = e.height = e.aboveeye = d.curradius;
+        if(collide(&e, vec(0, 0, 0), 0, false)) continue;
 
         int insert = 0;
         loopvrev(closedynlights) if(d.dist >= closedynlights[i]->dist) { insert = i+1; break; }
