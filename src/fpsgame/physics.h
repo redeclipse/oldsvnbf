e432200ce5043b4d82f09bc2a00aa6c34e546cf1
Lee Salzman <lsalzman@gmail.com>
2008-03-15 01:56:32 +0000

    entinmap message fix
    Former-commit-id: 218a7c7f8f9d9b140f118d6068fd7f8bd0a2f7dd

--- src/fpsgame/physics.h
+++ src/fpsgame/physics.h
@@ -699,7 +699,7 @@ struct physics
 			d->o.y += (rnd(21)-10)*i/5;
 			d->o.z += (rnd(21)-10)*i/5;
 		}
-		conoutf("can't find entity spawn spot! (%d, %d)", d->o.x, d->o.y);
+        conoutf("can't find entity spawn spot! (%.1f, %.1f, %.1f)", d->o.x, d->o.y, d->o.z);
 		// leave ent at original pos, possibly stuck
 		d->o = orig;
 		return false;
