b7a4830fbb223f2f451cae30f127e68cff815324
Lee Salzman <lsalzman@gmail.com>
2008-04-12 01:05:57 +0000

    tweaked depthfx defaults
    Former-commit-id: a5f777ecea2fe56c93ab53149af21057d1fa4661

--- src/engine/renderparticles.cpp
+++ src/engine/renderparticles.cpp
@@ -13,7 +13,7 @@ VAR(depthfxbias, 0, 1, 64);
 
 extern void cleanupdepthfx();
 VARFP(fpdepthfx, 0, 1, 1, cleanupdepthfx());
-VARFP(depthfxprecision, 0, 1, 1, cleanupdepthfx());
+VARFP(depthfxprecision, 0, 0, 1, cleanupdepthfx());
 VARFP(depthfxsize, 6, 7, 10, cleanupdepthfx());
 VARP(depthfx, 0, 1, 1);
 VARP(blurdepthfx, 0, 1, 7);
