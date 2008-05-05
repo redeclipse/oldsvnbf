67142f470892961efa55b1f57ea95ee52cc57e4f
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-05-05 19:06:32 +0000

    skybox clipping fixes
    Former-commit-id: 95d5998c0317e691d5c54ba05a94b41e48fb8985

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -883,7 +883,7 @@ void drawcubemap(int size, const vec &o, float yaw, float pitch, const cubemapsi
 
 	glClear(GL_DEPTH_BUFFER_BIT);
 
-	int farplane = max(max(fog*2, 384), hdr.worldsize*2);
+	int farplane = hdr.worldsize*2;
 
     project(90.0f, 1.0f, farplane, !side.flipx, !side.flipy, side.swapxy);
 
@@ -1180,7 +1180,7 @@ void gl_drawframe(int w, int h)
         aspect += blend*sinf(lastmillis/1000.0+PI)*0.1f;
     }
 
-	farplane = max(max(fog*2, 384), hdr.worldsize*2);
+    farplane = hdr.worldsize*2;
 
 	project(fovy, aspect, farplane);
 	transplayer();
