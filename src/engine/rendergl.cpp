58bb6241d46b13bd35bd58f6eaea26daaae45fa6
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-05-19 00:54:22 +0000

    microsoft opengl check
    Former-commit-id: 9c34625daff9f49e0a5b94d79fd65505579fe7ae

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -267,7 +267,7 @@ void gl_checkextensions()
 
         ati_texgen_bug = 1;
     }
-    else if(strstr(vendor, "Tungsten") || strstr(vendor, "Mesa"))
+    else if(strstr(vendor, "Tungsten") || strstr(vendor, "Mesa") || strstr(vendor, "Microsoft"))
     {
         avoidshaders = 1;
         floatvtx = 1;
