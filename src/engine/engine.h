fab01063a0c914c9c75dab683724fc70bda15189
Lee Salzman <lsalzman@gmail.com>
2008-05-21 02:45:10 +0000

    reflection clipping cleanups
    Former-commit-id: 0b7c27af98ecf183809b898e6e07600234364eba

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -173,7 +173,7 @@ static inline bool pvsoccluded(const ivec &bborigin, int size)
 extern bool hasVBO, hasDRE, hasOQ, hasTR, hasFBO, hasDS, hasTF, hasBE, hasCM, hasNP2, hasTC, hasTE, hasMT, hasD3, hasstencil, hasAF, hasVP2, hasVP3, hasPP, hasMDA, hasTE3, hasTE4, hasVP, hasFP, hasGLSL, hasGM;
 
 extern bool envmapping, renderedgame;
-extern GLfloat mvmatrix[16], projmatrix[16], mvpmatrix[16];
+extern GLfloat mvmatrix[16], projmatrix[16], mvpmatrix[16], invmvmatrix[16];
 
 extern bool hascursor;
 extern float cursorx, cursory;
