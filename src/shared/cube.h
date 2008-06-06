6b66010363732c447a9a6ff615de54c043ebb8fb
Quinton Reeves <qreeves@gmail.com>
2008-06-06 10:42:05 +0000

    engine version updates
    Former-commit-id: d67d36a2a07a0efdce2a1b0755804c3768429582

--- src/shared/cube.h
+++ src/shared/cube.h
@@ -1,9 +1,3 @@
-#define ENG_VERSION		62					// engine version, integer is divided by 100.f
-#define ENG_NAME		"Blood Frontier"	// engine namn
-#define ENG_RELEASE		"Alpha 2"			// engine release name
-
-#define MASTER_PORT		28800
-
 #include "tools.h"
 #include "command.h"
 #include "geom.h"
