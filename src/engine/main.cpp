f83d807fbf6008246620f9572d4bc99aabd9277f
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-06-16 05:49:57 +0000

    interceptkey fix
    Former-commit-id: 72fd726fa0f5d549db602f7a642fe8dbb3a58ea9

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -450,7 +450,7 @@ bool interceptkey(int sym)
         switch(event.type)
         {
         case SDL_KEYDOWN:
-            if(event.key.keysym.sym == SDLK_ESCAPE)
+            if(event.key.keysym.sym == sym)
                 return true;
 
         default:
