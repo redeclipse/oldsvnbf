2caa203819cc864a8a6f176fd1d1a67a28c6bc1b
Quinton Reeves <qreeves@gmail.com>
2008-06-18 01:00:39 +0000

    usage segfault and align fix
    Former-commit-id: b577b9ef7d4e9f23d99884a0ec591116aa50c842

--- src/shared/ttf2font.cpp
+++ src/shared/ttf2font.cpp
@@ -396,10 +396,10 @@ void makefont()
 
 void usage()
 {
-	conoutf("%s usage:\n");
+	conoutf("%s usage:\n", program);
 	conoutf("\t-?\t\tthis help");
 	conoutf("\t-h<S>\t\thome dir, output directory");
-	conoutf("\t-g<N[0..3]>\t\tgame type");
+	conoutf("\t-g<N[0..3]>\tgame type");
 	conoutf("\t-f<S>\t\tfont file");
 	conoutf("\t-n<S>\t\toutput name");
 	conoutf("\t-r\t\treset character mapping");
@@ -408,9 +408,9 @@ void usage()
 	conoutf("\t-s<N>\t\tfont size");
 	conoutf("\t-p<N>\t\tchar padding");
 	conoutf("\t-d<N>\t\tshadow depth");
-	conoutf("\t-o<N[0..1]>\t\toutward shadow");
+	conoutf("\t-o<N[0..1]>\toutward shadow");
 	conoutf("\t-q<N[0..2]>\trender quality");
-	conoutf("\t-z<N[0..9]>\tcompress level", program);
+	conoutf("\t-z<N[0..9]>\tcompress level");
 }
 
 int main(int argc, char *argv[])
