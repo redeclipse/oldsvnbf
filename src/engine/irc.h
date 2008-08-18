8b41c26af79dac42abeb1027801736a42e48dc20
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-08-18 14:48:29 +0000

    irc updates with the random screwiness that cropped up in ircjoin commented out
    Former-commit-id: 36418b8d05afa0b4c320bc5d2d78b49ded22f88f

--- src/engine/irc.h
+++ src/engine/irc.h
@@ -1,38 +1,31 @@
-
 enum { IRCC_NONE = 0, IRCC_JOINING, IRCC_JOINED, IRCC_KICKED, IRCC_BANNED };
+enum { IRCCT_NONE = 0, IRCCT_AUTO };
 struct ircchan
 {
-	int state;
-	char *name, *pass;
-	ircchan() : state(IRCC_NONE) {}
-	~ircchan() { DELETEP(name); DELETEP(pass); }
+	int state, type;
+	char *name, *passkey;
+
+	ircchan() {}
+	~ircchan() { DELETEP(name); DELETEP(passkey); }
 };
 enum { IRCT_NONE = 0, IRCT_CLIENT, IRCT_RELAY, IRCT_MAX };
 enum { IRC_DISC = 0, IRC_ATTEMPT, IRC_CONN, IRC_ONLINE, IRC_MAX };
 struct ircnet
 {
 	int type, state, port;
-	char *name, *serv, *nick, *pass;
+	char *name, *serv, *nick, *passkey;
 	ENetAddress address;
 	ENetSocket sock;
 	uchar input[4096];
 	vector<ircchan> channels;
 
 	ircnet() {}
-	ircnet(int t, const char *n, const char *s, int p, const char *c, const char *z = NULL)
-		: type(t), state(IRC_DISC), port(p), name(newstring(n)), serv(newstring(s)),
-			nick(newstring(c)), pass(newstring(z ? z : ""))
-	{
-		address.host = ENET_HOST_ANY;
-		address.port = port;
-		channels.setsize(0);
-		conoutf("added irc network %s (%s:%d) [%s]", name, serv, port, nick);
-	}
 	~ircnet()
 	{
-		DELETEP(name); DELETEP(serv); DELETEP(nick); DELETEP(pass);
+		DELETEP(name); DELETEP(serv); DELETEP(nick); DELETEP(passkey);
 		channels.setsize(0);
 	}
 };
-extern vector<ircnet *> ircnets;
+extern vector<ircnet> ircnets;
+extern void irccleanup();
 extern void ircslice();
