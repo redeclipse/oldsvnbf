bcc460f9b511dcef2df3e37d6dd6a7931620bb94
Lee Salzman <lsalzman@gmail.com>
2008-10-16 12:17:24 +0000

    single frame clip texs
    Former-commit-id: e2f35c65b83dd188c45636d15434aebc72a287da

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -660,4 +660,8 @@ extern void renderradius(vec &o, float xradius, float yradius, float zradius, bo
 
 extern bool rendericon(const char *icon, int x, int y, int xs = 120, int ys = 120);
 #define rendernormally ((!shadowmapping && !envmapping && !reflecting && !refracting) || glaring)
+
+extern void drawslice(float start, float length, float x, float y, float size);
+extern void drawfadedslice(float start, float length, float x, float y, float size, float alpha, float minsize = 0.25f);
+
 #endif // STANDALONE
