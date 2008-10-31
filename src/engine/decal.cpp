8131b2dcadca4bd1901e3a56c5d3a0b18f306888
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2008-10-31 06:25:05 +0000

    fade in energy decals faster
    Former-commit-id: 41fe976d064c6f70192eb0033947f2fe41d821f7

--- src/engine/decal.cpp
+++ src/engine/decal.cpp
@@ -497,7 +497,7 @@ decalrenderer decals[] =
     decalrenderer("particles/scorch", DF_ROTATE, 500, 1000, 5000),
     decalrenderer("particles/blood", DF_RND4|DF_ROTATE|DF_INVMOD, 0, 1000, 5000),
     decalrenderer("<decal>particles/bullet", DF_OVERBRIGHT, 0, 1000, 5000),
-    decalrenderer("particles/energy", DF_ROTATE|DF_ADD|DF_SATURATE, 250, 500, 1000)
+    decalrenderer("particles/energy", DF_ROTATE|DF_ADD|DF_SATURATE, 150, 500, 1000)
 };
 
 void initdecals()
