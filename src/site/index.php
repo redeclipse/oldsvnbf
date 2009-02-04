79bab169b92fdac0b8dc8246349fcc5f784df3a9
Quinton Reeves <qreeves@gmail.com>
2009-02-04 05:28:21 +0000

    fix some typos
    Former-commit-id: acd009c42a757c655c248d5535d45b1bec9c08f6

--- src/site/index.php
+++ src/site/index.php
@@ -2,6 +2,7 @@
 	$app['sitename'] = "Blood Frontier";
 	$app['siteblurb'] = "It's Bloody Fun!";
 	$app['sitevideo'] = "http://www.youtube.com/v/u8zika049FY&hl=en&fs=1";
+	$app['sitevurl'] = "http://www.youtube.com/watch?v=u8zika049FY";
 	$app['siteinfo'] = "In the distant future, humanity has spread throughout the solar system, to Mars and beyond. A vast communications network bridges from colony to colony, human to machine, and machine to human. This seemingly benign keystone of modern inter-planetary society, however, appears to be the carrier of a mysterious techno-biological plague. Any persons so-connected seem to fall ill and die, only to return as ravenous, sub-human cannibals. You, a machine intelligence, an android, remain unafflicted by this strange phenomenon and have been tasked with destroying the growing hordes of the infected, while, hopefully, locating and stopping the source of the epidemic.";
 	$app['sitelogo'] = "bits/logo.png";
 	$app['sitecss'] = "bits/site.css";
@@ -139,11 +140,11 @@
 <?php	} else { ?>
 			<div id="portal" align="center">
 				<p id="supertext" align="center"><a href="project"><?php echo $app['sitename']; ?></a>, <i><?php echo $app['siteblurb']; ?></i></p>
-				<div id="video" align="center">
+				<p id="video" align="center">
 					<object id="flash" type="application/x-shockwave-flash" data="<?php echo $app['sitevideo']; ?>">
 						<param name="movie" value="<?php echo $app['sitevideo']; ?>" />
 						<param name="allowscriptaccess" value="always" />
-						<a href="<?php echo $app['sitevideo']; ?>"><img src="<?php echo $app['sitelogo']; ?>" alt="" /></a>
+						<a href="<?php echo $app['sitevurl']; ?>"><img src="<?php echo $app['sitelogo']; ?>" alt="<?php echo $app['sitename']; ?> Video" /></a>
 					</object>
 				</p>
 				<p id="subtext" align="center"><?php echo $app['siteinfo']; ?></p>
