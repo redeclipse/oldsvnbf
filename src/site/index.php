0c8f5a20750d5c7c35555366f4c931cf0bf0d49e
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-02-05 05:25:52 +0000

    url formatting fix
    Former-commit-id: 6b37243591ef42f91a1d97632b3fe032cef6c472

--- src/site/index.php
+++ src/site/index.php
@@ -3,9 +3,9 @@
 	$app['siteblurb'] = "It's Bloody Fun!";
 	$app['sitevideo'] = "http://www.youtube.com/v/u8zika049FY&color1=0x000000&color2=0x000000&fs=1&egm=0&showsearch=0&showinfo=0&feature=PlayList&p=E7D81AC7DED5E1E3&index=0&playnext=0";
 	$app['siteinfo'] = "In the distant future, humanity has spread throughout the solar system, to Mars and beyond. A vast communications network bridges from colony to colony, human to machine, and machine to human. This seemingly benign keystone of modern inter-planetary society, however, appears to be the carrier of a mysterious techno-biological plague. Any persons so-connected seem to fall ill and die, only to return as ravenous, sub-human cannibals. You, a machine intelligence, an android, remain unafflicted by this strange phenomenon and have been tasked with destroying the growing hordes of the infected, while, hopefully, locating and stopping the source of the epidemic.";
-	$app['sitelogo'] = "bits/logo.png";
-	$app['sitecss'] = "bits/site.css";
-	$app['siteico'] = "bits/favicon.ico";
+	$app['sitelogo'] = "/bits/logo.png";
+	$app['sitecss'] = "/bits/site.css";
+	$app['siteico'] = "/bits/favicon.ico";
 
 	$app['sfproject'] = "bloodfrontier";
 	$app['sfgroupid'] = 198419;
@@ -41,7 +41,7 @@
 	foreach ($app['targets'] as $key => $targ) {
 		if ($key != "" && $targ['name'] != "") {
 			if ($app['navbar'] != "") $app['navbar'] .= ' | ';
-			$app['navbar'] .= '<a href="'.$key.'">'. $targ['name'] .'</a>';
+			$app['navbar'] .= '<a href="/'.$key.'">'. $targ['name'] .'</a>';
 		}
 	}
 
@@ -91,19 +91,19 @@
 			<div id="tags">
 				<div id="flink">
 <?php				if ($app['sfdonate'] > 0) { ?>
-					<a href="donate">
+					<a href="/donate">
 						<img id="sfdonate" src="http://images.sourceforge.net/images/project-support.jpg" style="border: none" alt="Support <?php echo $app['sitename']; ?> and Donate" />
 					</a>
 <?php				} 
 					if ($app['sflogo'] > 0) { ?>
-					<a href="project">
+					<a href="/project">
 						<img id="sflogo" src="http://sflogo.sourceforge.net/sflogo.php?group_id=<?php echo $app['sfgroupid']; ?>&type=<?php echo $app['sflogo']; ?>" style="border: none" alt="Get <?php echo $app['sitename']; ?> at SourceForge" />
 					</a>
 <?php				} ?>
 				</div>
 				<div id="blurb"><i><?php echo $app['target'] != 'home' ? "Loading Application.." : $app['siteblurb']; ?></i></div>
 			</div>
-			<div id="logo"><a href="home"><img id="sitelogo" src="<?php echo $app['sitelogo']; ?>" alt="<?php echo $app['sitename']; ?>" style="border: none" /></a></div>
+			<div id="logo"><a href="/home"><img id="sitelogo" src="<?php echo $app['sitelogo']; ?>" alt="<?php echo $app['sitename']; ?>" style="border: none" /></a></div>
 			<div id="navbar"><?php echo $app['navbar']; ?></div>
 		</div>
 		<div id="body" align="center">
@@ -113,7 +113,7 @@
 			</iframe>
 <?php	} else { ?>
 			<div id="portal" align="center">
-				<p id="supertext" align="center"><a href="project"><?php echo $app['sitename']; ?></a>, <i><?php echo $app['siteblurb']; ?></i></p>
+				<p id="supertext" align="center"><a href="/project"><?php echo $app['sitename']; ?></a>, <i><?php echo $app['siteblurb']; ?></i></p>
 				<p id="video" align="center">
 					<object id="flash" type="application/x-shockwave-flash" data="<?php echo $app['sitevideo']; ?>">
 						<param name="movie" value="<?php echo $app['sitevideo']; ?>" />
@@ -123,7 +123,7 @@
 					</object>
 				</p>
 				<p id="subtext" align="center"><?php echo $app['siteinfo']; ?></p>
-				<p id="footer" align="center"><a href="download">Download</a>, <a href="wiki">Learn More</a>, <a href="forums">Get Help</a>, or <a href="chat">Join In</a> today!</p>
+				<p id="footer" align="center"><a href="/download">Download</a>, <a href="/wiki">Learn More</a>, <a href="/forums">Get Help</a>, or <a href="/chat">Join In</a> today!</p>
 			</div>
 <?php	} ?>
 		</div>
