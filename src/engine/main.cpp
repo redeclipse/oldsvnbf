ed61afce14c3b7d575e6be4ae2f659f45a83e0ca
Quinton Reeves <qreeves@gmail.com>
2009-02-14 19:33:08 +0000

    it's a secret.
    Former-commit-id: 0255a62a77218c1fba43d9088d6c1e220d8e23e2

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -688,25 +688,20 @@ void eastereggs()
 	tm_isdst	1 if daylight savings is on, zero if not,
 	*/
 
-	if(lt->tm_wday == 5 && lt->tm_mday == 13)		// Friday the 13th
+	if(lt->tm_wday == 5 && lt->tm_mday == 13)
 	{
 		loadback = "textures/spookyback";
 		loadbackinfo = "Friday the 13th";
 	}
-	else if(lt->tm_mon == 9 && lt->tm_mday == 31)	// Halloween
+	else if(lt->tm_mon == 9 && lt->tm_mday == 31)
 	{
 		loadback = "textures/spookyback";
 		loadbackinfo = "Halloween";
 	}
 
-	if(lt->tm_mon == 7 && lt->tm_mday == 26)		// Acord's Birthday
-	{
-		loadbackinfo = "Happy Birthday Acord!";
-	}
-	if(lt->tm_mon == 2 && lt->tm_mday == 9)			// Quin's Birthday
-	{
-		loadbackinfo = "Happy Birthday Quin!";
-	}
+	if(lt->tm_mon == 7 && lt->tm_mday == 26)	loadbackinfo = "Happy Birthday Acord!";
+	if(lt->tm_mon == 2 && lt->tm_mday == 9)		loadbackinfo = "Happy Birthday Quin!";
+	if(lt->tm_mon == 9 && lt->tm_mday == 26)	loadbackinfo = "Happy Birthday Dazza!";
 }
 
 bool findoctadir(const char *name)
