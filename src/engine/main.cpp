5e537bf1893928fea14cc968ee807f7bfb242857
Hirato <billy65bob@gmail.com>
2009-03-06 13:12:50 +0000

    corrected the date for ahven's b'day
    Former-commit-id: 68ab6474151834ca265908e4e84090bd841332b9

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -730,7 +730,7 @@ void eastereggs()
 		loadback = "textures/spookyback";
 		loadbackinfo = "Halloween";
 	}
-	if(month == 2 && mday == 6)		loadbackinfo = "Happy Birthday Ahven!";
+	if(month == 1 && mday == 6)		loadbackinfo = "Happy Birthday Ahven!";
 	if(month == 2 && mday == 9)		loadbackinfo = "Happy Birthday Quin!";
 	if(month == 4 && mday == 18)	loadbackinfo = "Happy Birthday Geartrooper!";
 	if(month == 7 && mday == 26)	loadbackinfo = "Happy Birthday Acord!";
