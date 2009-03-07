b35ad695eaecccf8744cb5a6ea6c4141da51bd04
Quinton Reeves <qreeves@gmail.com>
2009-03-07 21:08:06 +0000

    fix texture positions and flamer smoke
    Former-commit-id: 2235e3889016affea9d4e66832d80de5a8fe532e

--- src/game/stf.cpp
+++ src/game/stf.cpp
@@ -78,10 +78,10 @@ namespace stf
 			loopv(st.flags) if(insideflag(st.flags[i], world::player1) && (st.flags[i].owner == world::player1->team || st.flags[i].enemy == world::player1->team))
 			{
 				pushfont("emphasis");
+				ty -= draw_textx("Team [ \fs%s%s\fS ]", tx-FONTH-FONTH/2, ty, 255, 255, 255, int(255*hudblend), TEXT_RIGHT_UP, -1, -1, teamtype[world::player1->team].chat, teamtype[world::player1->team].name);
 				settexture(hud::flagtex(world::player1->team), 3);
 				glColor4f(1.f, 1.f, 1.f, int(255*hudblend));
 				hud::drawsized(tx-FONTH, ty, FONTH);
-				ty -= draw_textx("Team [ \fs%s%s\fS ]", tx-FONTH-FONTH/2, ty, 255, 255, 255, int(255*hudblend), TEXT_RIGHT_UP, -1, -1, teamtype[world::player1->team].chat, teamtype[world::player1->team].name);
 				popfont();
 				pushfont("default");
 				int occupy = int((st.flags[i].enemy ? clamp(st.flags[i].converted/float((st.flags[i].owner ? 2 : 1)*st.OCCUPYLIMIT), 0.f, 1.f) : (st.flags[i].owner ? 1.f : 0.f))*100.f);
