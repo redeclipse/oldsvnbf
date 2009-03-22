3d3f9912c9ab26300f89eb407f3a01a098e8d629
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-03-22 06:15:39 +0000

    emboldened navbar, added support for dynamically included body content
    Former-commit-id: 0d527ae1ede75815518edebd915956d17690d638

---
+++ src/site/home.php
@@ -0,0 +1,11 @@
+<p id="supertext" align="center"><a href="/project"><?php echo $app['sitename']; ?></a>, <i><?php echo $app['siteblurb']; ?></i></p>
+<p id="video" align="center">
+	<object id="flash" type="application/x-shockwave-flash" data="<?php echo $app['sitevideo']; ?>">
+		<param name="movie" value="<?php echo $app['sitevideo']; ?>" />
+		<param name="allowscriptaccess" value="always" />
+		<param name="allowFullScreen" value="true" />
+		<embed id="flash" src="<?php echo $app['sitevideo']; ?>" type="application/x-shockwave-flash" allowfullscreen="true"></embed>
+	</object>
+</p>
+<p id="subtext" align="center"><?php echo $app['siteinfo']; ?></p>
+<p id="footer" align="center"><a href="/download">Download</a>, <a href="/wiki">Learn More</a>, <a href="/forums">Get Help</a>, or <a href="/chat">Join In</a> today!</p>
