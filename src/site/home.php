aa644609f08e9570474dd2e1642d4aed0e6fa343
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-04-06 07:18:43 +0000

    site updates
    Former-commit-id: 870dd0ddb5a476b1741b3d6cd354c4b101436ef3

--- src/site/home.php
+++ src/site/home.php
@@ -7,5 +7,5 @@
 		<embed id="flash" src="<?php echo $app['sitevideo']; ?>" type="application/x-shockwave-flash" allowfullscreen="true"></embed>
 	</object>
 </p>
-<p id="subtext" align="center"><?php echo $app['siteinfo']; ?></p>
+<p id="subtext" align="center"><i><?php echo $app['siteinfo']; ?></i></p>
 <p id="footer" align="center"><a href="/download">Download</a>, <a href="/wiki">Learn More</a>, <a href="/forums">Get Help</a>, or <a href="/chat">Join In</a> today!</p>
