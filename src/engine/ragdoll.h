40a30d61a57cd0c9e519d4ddd0ef5c0723c72df9
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-04-08 04:51:13 +0000

    give ragdoll longer time to live under water
    Former-commit-id: dd529a96fa1dd7a229449a96f32926f6de9e6f0e

--- src/engine/ragdoll.h
+++ src/engine/ragdoll.h
@@ -311,6 +311,7 @@ FVAR(ragdollgroundfric, 0, 0.8f, 1);
 FVAR(ragdollairfric, 0, 0.996f, 1);
 FVAR(ragdollgravity, 0, 1.f, 1000);
 VAR(ragdollexpireoffset, 0, 1000, 30000);
+VAR(ragdollwaterexpireoffset, 0, 3000, 30000);
 VAR(ragdollexpiremillis, 1, 1000, 30000);
 VAR(ragdolltimestepmin, 1, 5, 50);
 VAR(ragdolltimestepmax, 1, 13, 50);
@@ -354,7 +355,7 @@ void ragdolldata::move(dynent *pl, float ts)
     if(collisions)
     {
         floating = 0;
-        if(!collidemillis) collidemillis = lastmillis + ragdollexpireoffset;
+        if(!collidemillis) collidemillis = lastmillis + (water ? ragdollwaterexpireoffset : ragdollexpireoffset);
     }
     else if(++floating > 1 && lastmillis < collidemillis + ragdollexpiremillis) collidemillis = 0;
 
