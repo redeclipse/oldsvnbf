b813e6860d19adbeefff7ef4661637155e1b0634
Quinton Reeves <qreeves@gmail.com>
2009-04-09 14:51:23 +0000

    turned donation begging notice into a thank you notice
    Former-commit-id: 829a37d0e849c994e6f5b411ff7030b81b35825c

--- src/site/home.php
+++ src/site/home.php
@@ -1,9 +1,21 @@
-<p id="right" align="center">
+<div id="notice" align="center">
+	<u>Thanks</u> to all the donors following our last release!
+	Quin successfully raised half the money required to fund the contruction of a new PC, which has arrived and been (stress) tested.
+	Our heartfelt appreciation goes to all those who helped ensure our Lead Developer can continue his tireless work for a long time to come.
+	If you'd still like to donate, further money raised will go toward future project endeavours and ongoing costs.
+	Donors get to be listed in our <a href="http://www.bloodfrontier.com/wiki/Credits">Credits</a> and receive other project perks like weighted decisions.
+</div>
+<div id="right" align="center">
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="212900">
+		<input type="image" src="https://www.paypal.com/en_AU/i/btn/btn_donate_LG.gif" name="submit" alt="Donate to Quin">
+	</form>
+</div>
+<div id="right" align="center">
 	<a href="http://www.xtremetop100.com/in.php?site=1132279338">
 		<img src="http://www.xtremeTop100.com/votenew.jpg" border="0" alt="Vote for Blood Frontier at XtremeTop100" />
 		<div id="insert" align="center">Vote for Us!</div>
 	</a>
-</p>
+</div>
 <p id="supertext" align="center"><a href="/project"><?php echo $app['sitename']; ?></a>, <i><?php echo $app['siteblurb']; ?></i></p>
 <p id="video" align="center">
 	<object id="flash" type="application/x-shockwave-flash" data="<?php echo $app['sitevideo']; ?>">
