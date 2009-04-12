1afae51d1e6a638350274ff7a7e406dfc448e4a3
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-04-12 10:29:08 +0000

    don't sort when shadowmapping
    Former-commit-id: 01fcdb5fd48e957f24f15cdb57ea3592c9dd896b

--- src/engine/rendermodel.cpp
+++ src/engine/rendermodel.cpp
@@ -552,7 +552,7 @@ void endmodelbatches()
 				query = bm.query;
                 if(query) startquery(query);
 			}
-            if(bm.transparent < 1 && (!query || query->owner==bm.d))
+            if(bm.transparent < 1 && (!query || query->owner==bm.d) && !shadowmapping)
 			{
 				transparentmodel &tm = transparent.add();
 				tm.m = b.m;
