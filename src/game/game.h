93e4586821d692900d6cbd237f4d07314ea1ed47
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-10-16 12:24:09 +0000

    remove impact player from flamer primary action
    Former-commit-id: 57fae3bff13bf3a935aaaaba87eaaae907ef7463

--- src/game/game.h
+++ src/game/game.h
@@ -259,7 +259,7 @@ weaptypes weaptype[WEAP_MAX] =
 		WEAP_FLAMER,		ANIM_FLAMER,		0xFF2222,		S_FLAMER,	S_BURN,		S_BURNING,	-1,
 			50,		50,		{ 1, 5 },	{ 100, 750 }, 	2000,	{ 5, 5 },		{ 150, 250 },		0,			{ 350, 700 },
 			0,		{ 28, 32 },	{ 1, 5 },		{ 40, 10 },		{ 2, 2 },
-			{ BOUNCE_GEOM|IMPACT_PLAYER, IMPACT_GEOM|IMPACT_PLAYER|COLLIDE_OWNER },
+			{ BOUNCE_GEOM, IMPACT_GEOM|IMPACT_PLAYER|COLLIDE_OWNER },
 			{ true, true },		{ false, false },	{ true, true },		{ true, true, },	true,		false,	{ true, true },		{ false, false },
 			{ 0.15f, 0.f },		{ 45, 0 },			{ 0.75f, 0.5f },	{ 1, 1 },		{ -300, 75 },		{ 30, 32 },			{ 0, 0 },
 			{ 0.25f, 1 },		{ 20, 40 },			{ 54, 128 },		7,
