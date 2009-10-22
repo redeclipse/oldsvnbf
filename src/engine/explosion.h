a619b3411648b5400d362900e76be1dfad020d3b
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-10-22 10:30:11 +0000

    explosion culling fix
    Former-commit-id: 949635d603761a7bdfa46a624a20829fc1bd5399

--- src/engine/explosion.h
+++ src/engine/explosion.h
@@ -431,7 +431,7 @@ struct fireballrenderer : sharedlistrenderer
                isvisiblesphere(psize, p->o) >= VFC_FOGGED) continue;
 
             e.o = p->o;
-            e.radius = e.height = e.aboveeye = psize;
+            e.radius = e.xradius = e.yradius = e.height = e.aboveeye = psize;
             if(::collide(&e, vec(0, 0, 0), 0, false)) continue;
 
             if(depthfxscissor==2 && !depthfxtex.addscissorbox(p->o, psize)) continue;
