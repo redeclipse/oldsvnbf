ce34496b9e26d514d1d9f978da9e56ad54d8ce14
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-11-13 00:11:36 +0000

    fixed echo statement
    Former-commit-id: 0555c01ac55911d5332c5e5d50a75525b993add1

--- server.bat
+++ server.bat
@@ -11,7 +11,7 @@ IF EXIST bin\bfclient.exe (
 		bin\bfserver.exe %BF_OPTIONS% %*
 		popd
 	) ELSE (
-		echo Unable to find the Blood Frontier client
+		echo Unable to find the Blood Frontier server binary
 		pause
 	)
 )
