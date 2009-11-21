76b8a3c0bc3f8f7428be942f4c9adc806698ed20
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-11-21 16:26:33 +0000

    use bfserver.exe instead of bfclient.exe
    Former-commit-id: 020084e63396ac807cb300895f7c23b24a28f8e7

--- server.bat
+++ server.bat
@@ -3,8 +3,8 @@
 set BF_DIR=.
 set BF_OPTIONS=-sc8
 
-IF EXIST bin\bfclient.exe (
-	bin\bfclient.exe %BF_OPTIONS% %* 
+IF EXIST bin\bfserver.exe (
+	bin\bfserver.exe %BF_OPTIONS% %* 
 ) ELSE (
 	IF EXIST %BF_DIR%\bin\bfserver.exe (
 		pushd %BF_DIR%
