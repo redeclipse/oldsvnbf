d618cc5159f59fe03163ff7e6254c1ed39315e0e
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-11-29 14:45:32 +0000

    pickspawn fix
    Former-commit-id: 057e28830c2344db53e6f337de03816459f9b8ff

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -969,7 +969,7 @@ namespace server
 							loopv(spawns[team].cycle) if(spawns[team].cycle[i] == lowest) num++;
 							if(num > 0)
 							{
-								int r = rnd(num+1), n = 0;
+								int r = rnd(num), n = 0;
 								loopv(spawns[team].cycle) if(spawns[team].cycle[i] == lowest)
 								{
 									if(n == r)
