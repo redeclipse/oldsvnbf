852bb89a5e1daaaa4277d8e2f3c28ca340e67b65
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-11-30 04:17:23 +0000

    make /connect without parameters connect to the default server
    Former-commit-id: 453b7abbfca2a7b63c8db13db57e7f2ef6cd7022

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -8,10 +8,10 @@
 #define ENG_RELEASE			"Beta 2"
 #define ENG_URL				"www.bloodfrontier.com"
 #define ENG_BLURB			"It's bloody fun!"
-#define ENG_DEVEL			false
+#define ENG_DEVEL			true
 
-#define ENG_SERVER_PORT		28795
-#define ENG_QUERY_PORT		28796
+#define ENG_SERVER_PORT		(ENG_DEVEL ? 28797 : 28795)
+#define ENG_QUERY_PORT		(ENG_DEVEL ? 28798 : 28796)
 #define ENG_MASTER_PORT		28800
 #define ENG_MASTER_HOST		"play.bloodfrontier.com"
 
