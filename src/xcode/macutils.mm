b4ff4eeaa116a84d569b61c0c454908025fbff5b
rbrtpntn <rbrtpntn@0d5a7262-033b-0410-87d9-8ba89202c25b>
2009-12-07 07:45:23 +0000

    find directories
    Former-commit-id: 3107562a12cd2fa1491f336050108b448e54a323

--- src/xcode/macutils.mm
+++ src/xcode/macutils.mm
@@ -1,8 +1,11 @@
 #import <Cocoa/Cocoa.h>
 
-#define MAXSTRLEN 260
-inline char *s_strncpy(char *d, const char *s, size_t m) { strncpy(d,s,m); d[m-1] = 0; return d; };
-inline char *s_strcat(char *d, const char *s) { size_t n = strlen(d); return s_strncpy(d+n,s,MAXSTRLEN-n); };
+// -- copied from tools.h -- including the full file introduces too many problems
+#define MAXSTRLEN 512
+typedef char string[MAXSTRLEN];
+inline char *copystring(char *d, const char *s, size_t len = MAXSTRLEN) { strncpy(d, s, len); d[len-1] = 0; return d; }
+inline char *concatstring(char *d, const char *s) { size_t len = strlen(d); return copystring(d+len, s, MAXSTRLEN-len); }
+// --
 
 void mac_pasteconsole(char *commandbuf)
 {	
@@ -11,7 +14,7 @@ void mac_pasteconsole(char *commandbuf)
     if (type != nil) {
         NSString *contents = [pasteboard stringForType:type];
         if (contents != nil)
-			s_strcat(commandbuf, [contents lossyCString]);
+			concatstring(commandbuf, [contents lossyCString]);
     }
 }
 
@@ -25,4 +28,24 @@ int mac_osversion()
     SInt32 MacVersion;
     Gestalt(gestaltSystemVersion, &MacVersion);
     return MacVersion;
+}
+
+const char *mac_personaldir() {
+    static string dir;
+    NSString *path = nil;
+    FSRef folder;
+    if (FSFindFolder(kUserDomain, kApplicationSupportFolderType, NO, &folder) == noErr) 
+    {
+        CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, &folder);
+        path = [(NSURL *)url path];
+        CFRelease(url);
+    }
+    return path ? copystring(dir, [path fileSystemRepresentation]) : NULL;
+}
+
+const char *mac_sauerbratendir() {
+    static string dir;
+    NSString *path = [[NSWorkspace sharedWorkspace] fullPathForApplication:@"sauerbraten"];
+    if(path) path = [path stringByAppendingPathComponent:@"Contents/gamedata"];
+    return path ? copystring(dir, [path fileSystemRepresentation]) : NULL;
 }
