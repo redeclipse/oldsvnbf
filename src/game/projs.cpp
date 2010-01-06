b8004255b361a069d8b40b9a46f83bea4eb9751f
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2010-01-06 02:58:37 +0000

    fix missed else case..
    Former-commit-id: cb7cd843c5eb506fceec59a415707459e24f47af

--- src/game/projs.cpp
+++ src/game/projs.cpp
@@ -1103,16 +1103,8 @@ namespace projs
 				{
 					float mag = proj.vel.magnitude(), amt = diff*mag/(proj.projtype == PRJ_EJECT ? 2.f : 5.f);
 					vec vel = vec(proj.vel).normalize(), trj; vecfromyawpitch(proj.yaw, 0, 1, 0, trj);
-					if(vel.x*vel.y >= 0 ? trj.x*trj.y >= 0 : trj.x*trj.y < 0)
-					{
-						proj.pitch -= amt;
-						while(proj.pitch < 0) proj.pitch += 360;
-					}
-					else
-					{
-						proj.pitch += diff*mag/2.f;
-						while(proj.pitch >= 360) proj.pitch -= 360;
-					}
+					if(vel.x*vel.y >= 0 ? trj.x*trj.y >= 0 : trj.x*trj.y < 0) { proj.pitch -= amt; while(proj.pitch < 0) proj.pitch += 360; }
+					else { proj.pitch += amt; while(proj.pitch >= 360) proj.pitch -= 360; }
 					break;
 				}
 				else if(proj.projtype != PRJ_EJECT) break;
