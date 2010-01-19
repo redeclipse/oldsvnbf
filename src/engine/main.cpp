08225e0b62c662a5fda4d6224bea4479f5aa7b88
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2010-01-19 14:53:31 +0000

    screenshot fixes
    Former-commit-id: f4f2a0f1cf13beda578a586f7b45fa5952e94a10

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -191,7 +191,7 @@ void screenshot(char *sname)
     ImageData image(screen->w, screen->h, 3);
     glPixelStorei(GL_PACK_ALIGNMENT, 1);
 	glReadPixels(0, 0, screen->w, screen->h, GL_RGB, GL_UNSIGNED_BYTE, image.data);
-	defformatstring(fname)("%s", sname && *sname ? sname : mapname);
+	defformatstring(fname)("%s", sname && *sname ? sname : "screenshot");
 	saveimage(fname, image, imageformat, compresslevel, true, true);
 }
 
