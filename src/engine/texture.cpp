36ba430cb0c759ffe5a28819fcfbb2e55382d404
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2010-02-06 03:07:32 +0000

    added <grey> texture name command for forcing greyscale
    Former-commit-id: 5ed718511cd8295b0886cd7c1440557c1e272b73

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -320,6 +320,14 @@ void texmix(ImageData &s, int c1, int c2, int c3, int c4)
     s.replace(d);
 }
 
+void texgrey(ImageData &s)
+{
+    if(s.bpp <= 1) return;
+    ImageData d(s.w, s.h, 1);
+    readwritetex(d, s, dst[0] = src[0]);
+    s.replace(d);
+}
+
 VAR(0, hwtexsize, 1, 0, 0);
 VAR(0, hwcubetexsize, 1, 0, 0);
 VAR(0, hwmaxaniso, 1, 0, 0);
@@ -951,6 +959,7 @@ static bool texturedata(ImageData &d, const char *tname, Slot::Tex *tex = NULL, 
         else if(!strncmp(cmd, "reorient", len)) texreorient(d, atoi(arg[0])>0, atoi(arg[1])>0, atoi(arg[2])>0, tex ? tex->type : TEX_DIFFUSE);
         else if(!strncmp(cmd, "crop", len)) texcrop(d, atoi(arg[0]), atoi(arg[1]), atoi(arg[2]), atoi(arg[3]));
         else if(!strncmp(cmd, "mix", len)) texmix(d, *arg[0] ? atoi(arg[0]) : -1, *arg[1] ? atoi(arg[1]) : -1, *arg[2] ? atoi(arg[2]) : -1, *arg[3] ? atoi(arg[3]) : -1);
+        else if(!strncmp(cmd, "grey", len)) texgrey(d);
         else if(!strncmp(cmd, "compress", len) || !strncmp(cmd, "dds", len))
         {
             int scale = atoi(arg[0]);
