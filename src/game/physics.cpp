e5a58c25440abc19f09f8c443086dee287d49a12
Lee Salzman <lsalzman@gmail.com>
2010-04-02 00:58:54 +0000

    step down fixes
    Former-commit-id: 0e59420e39b9eab4a2c7decb4fa64dd4ed450d1f

--- src/game/physics.cpp
+++ src/game/physics.cpp
@@ -310,14 +310,9 @@ namespace physics
         if(dir.dot(floor) >= 0)
         {
             if(d->physstate < PHYS_SLIDE || fabs(dir.dot(d->floor)) > 0.01f*dir.magnitude()) return;
+            d->vel.projectxy(floor, 0.0f);
         }
-        if(d->physstate < PHYS_SLIDE) d->vel.projectxy(floor);
-        else
-        {
-            float speed = d->vel.magnitude();
-            d->vel.projectxydir(floor);
-            d->vel.rescale(speed);
-        }
+        else d->vel.projectxy(floor);
         d->falling.project(floor);
         recalcdir(d, oldvel, dir);
     }
@@ -446,11 +441,29 @@ namespace physics
             d->zmargin = 0;
             if(collide(d, vec(0, 0, -1)))
             {
-                if(init) d->o = old;
-                stepdir.mul(-stepdir.z).z += 1;
-                stepdir.normalize();
-                switchfloor(d, dir, stepdir);
-                d->floor = stepdir;
+                vec stepfloor(stepdir);
+                stepfloor.mul(-stepfloor.z).z += 1;
+                stepfloor.normalize();
+                if(d->physstate >= PHYS_SLOPE && d->floor != stepfloor)
+                {
+                    // prevent alternating step-down/step-up states if player would keep bumping into the same floor 
+                    vec stepped(d->o);
+                    d->o.z -= 0.5f;
+                    d->zmargin = -0.5f;
+                    if(!collide(d, stepdir) && wall == d->floor)
+                    {
+                        d->o = old;
+                        if(!init) { d->o.x += dir.x; d->o.y += dir.y; if(dir.z <= 0 || !collide(d, dir)) d->o.z += dir.z; }
+                        d->zmargin = 0;
+                        d->physstate = PHYS_STEP_DOWN;
+                        return true;
+                    }
+                    d->o = init ? old : stepped;
+                    d->zmargin = 0;
+                }
+                else if(init) d->o = old;
+                switchfloor(d, dir, stepfloor);
+                d->floor = stepfloor;
                 if(init)
                 {
                     d->timeinair = 0;
@@ -590,7 +603,7 @@ namespace physics
                 d->o.add(dir);
             }
         }
-        else if((d->type == ENT_PLAYER || d->type == ENT_AI) && d->physstate == PHYS_STEP_DOWN)
+        else if((d->type == ENT_PLAYER || d->type == ENT_AI) && d->physstate == PHYS_STEP_DOWN && dir.dot(d->floor) <= 1e-6f)
         {
             vec moved(d->o);
             d->o = old;
