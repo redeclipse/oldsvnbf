32c6360d96b147148ff234994b2f35e41d1ac1da
Quinton Reeves <qreeves@gmail.com>
2010-04-09 13:48:30 +0000

    bins, spawnrotate tweak
    Former-commit-id: ef3ce3cc9433d75a46c45981f2af75fc74696a29

--- src/game/vars.h
+++ src/game/vars.h
@@ -40,7 +40,7 @@ GICOMMAND(IDF_ADMIN, resetconfig, "", (), rehash(true), );
 GVAR(0, maprotate, 0, 2, 2); // 0 = off, 1 = sequence, 2 = random
 
 GVAR(0, maxcarry, 1, 2, WEAP_MAX-1);
-GVAR(0, spawnrotate, 0, 4, INT_MAX-1); // 0 = let client decide, 1 = sequence, 2 = random
+GVAR(0, spawnrotate, 0, 4, INT_MAX-1); // 0 = let client decide, 1 = sequence, 2+ = random
 GVAR(0, spawnweapon, 0, WEAP_PISTOL, WEAP_MAX-1);
 GVAR(0, instaweapon, 0, WEAP_RIFLE, WEAP_MAX-1);
 GVAR(0, limitedweapon, 0, WEAP_MELEE, WEAP_MAX-1);
