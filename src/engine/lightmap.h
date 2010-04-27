eabcddbc3d49964b54a5675d30e8dd7812da0ef2
eihrul <eihrul@0d5a7262-033b-0410-87d9-8ba89202c25b>
2010-04-27 08:55:28 +0000

    thread fixes
    Former-commit-id: da5fbf63b0b9402d0f10506c1b5ce093932384f1

--- src/engine/lightmap.h
+++ src/engine/lightmap.h
@@ -122,16 +122,19 @@ extern void lerpnormal(float v, const lerpvert *lv, int numv, lerpbounds &start,
 extern void newnormals(cube &c);
 extern void freenormals(cube &c);
 
-#define CHECK_CALCLIGHT_PROGRESS(exit, show_calclight_lmprog) \
+#define CHECK_CALCLIGHT_PROGRESS_LOCKED(exit, show_calclight_lmprog, before, after) \
     if(check_calclight_lmprog) \
     { \
         if(!calclight_canceled) \
         { \
+            before; \
             show_calclight_lmprog(); \
             check_calclight_canceled(); \
+            after; \
         } \
-        if(calclight_canceled) exit; \
+        if(calclight_canceled) { exit; } \
     }
+#define CHECK_CALCLIGHT_PROGRESS(exit, show_calclight_lmprog) CHECK_CALCLIGHT_PROGRESS_LOCKED(exit, show_calclight_progress, , )
 
 extern bool calclight_canceled;
 extern volatile bool check_calclight_lmprog;
