c7ec185664000ebfda80f0033102f15530312cd0
Lee Salzman <lsalzman@gmail.com>
2010-05-12 22:11:32 +0000

    dirty skelcache fix
    Former-commit-id: ff32445e07298226f897930088f4c1bb8defa103

--- src/engine/skelmodel.h
+++ src/engine/skelmodel.h
@@ -1234,7 +1234,8 @@ struct skelmodel : animmodel
 
         void setasmbones(skelcacheentry &sc, int count = 0)
         {
-            if((count ? lastbdata : lastsdata) == (usematskel ? (void *)sc.mdata : (void *)sc.bdata)) return;
+            if(sc.dirty) sc.dirty = false;
+            else if((count ? lastbdata : lastsdata) == (usematskel ? (void *)sc.mdata : (void *)sc.bdata)) return;
             int offset = count ? numgpubones : 0;
             if(!offset) count = numgpubones;
             if(hasPP)
@@ -1261,7 +1262,7 @@ struct skelmodel : animmodel
         {
             if(hasUBO)
             {
-                if(!lastsdata && lastbdata == &bc.ubuf) return;
+                if(!lastsdata && lastbdata == &bc.ubuf && !bc.dirty) return;
             }
             else if(u.version == bc.version && u.data == &bc.ubuf) return;
             if(!bc.ubuf) { glGenBuffers_(1, &bc.ubuf); bc.dirty = true; }
