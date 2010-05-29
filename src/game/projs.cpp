2bfc83f783e7cfb1aa86ead48f0f9fa2594abf17
qreeves <qreeves@0d5a7262-033b-0410-87d9-8ba89202c25b>
2010-05-29 04:23:24 +0000

    enemymillis/enemyseen confusion fix
    Former-commit-id: d6f8a3e45d578e6a5bde399868991231b67605e1

--- src/game/projs.cpp
+++ src/game/projs.cpp
@@ -616,7 +616,7 @@ namespace projs
             create(from, from, local, d, PRJ_EJECT, rnd(ejectfade)+ejectfade, 0, millis, rnd(weaptype[weap].espeed)+weaptype[weap].espeed, 0, weap, flags);
 
         if(d->aitype >= AI_BOT && (!WEAP2(weap, fullauto, flags&HIT_ALT) || adelay >= PHYSMILLIS))
-            adelay += int(adelay*(20.f/d->skill));
+            adelay += int(adelay*(10.f/d->skill));
         d->setweapstate(weap, flags&HIT_ALT ? WEAP_S_SECONDARY : WEAP_S_PRIMARY, adelay, lastmillis);
         d->ammo[weap] = max(d->ammo[weap]-offset, 0);
         d->weapshot[weap] = offset;
